b6c030a3d313539a2930e38da04df580fa0816d1
zserg <zsergt3@gmail.com>
2015-12-25 12:04:36 +0300

    tests added for inverted lattitude/logitude

--- elodie/tests/media/photo_test.py
+++ elodie/tests/media/photo_test.py
@@ -77,6 +77,12 @@ def test_get_coordinate_latitude_minus():
 
     assert helper.isclose(coordinate,-37.3667027222), coordinate
 
+def test_get_coordinate_latitude_minus():
+    photo = Photo(helper.get_file('with-location-inv.jpg'))
+    coordinate = photo.get_coordinate('latitude')
+
+    assert coordinate == -37.3667027222, coordinate
+
 def test_get_coordinate_longitude():
     photo = Photo(helper.get_file('with-location.jpg'))
     coordinate = photo.get_coordinate('longitude')
@@ -88,6 +94,12 @@ def test_get_coordinate_longitude_plus():
     coordinate = photo.get_coordinate('longitude')
 
     assert helper.isclose(coordinate,122.033383611), coordinate
+
+def test_get_coordinate_longitude_plus():
+    photo = Photo(helper.get_file('with-location-inv.jpg'))
+    coordinate = photo.get_coordinate('longitude')
+
+    assert coordinate == 122.033383611, coordinate
 
 def test_get_coordinates_without_exif():
     photo = Photo(helper.get_file('no-exif.jpg'))
